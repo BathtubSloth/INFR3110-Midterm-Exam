b41a857367b4200e4bf059cc4df6d96d61bfdca6
Nathan Tuck <nathan.tuck1@ontariotechu.net>
2020-10-06 21:48:33 -0400

    Added Scene Switching and Button Functionality
    Added a simple scene switching function to allow the buttons to swap scenes.

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void SwitchSceneToStart()
+    {
+        SceneManager.LoadScene("Start Scene"); // from https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.LoadScene.html
+    }
+    
+    public void SwitchSceneToPlay()
+    {
+        SceneManager.LoadScene("Play Scene"); //same as above, different scene
+    }
+
+    public void SwitchSceneToEnd()
+    {
+        SceneManager.LoadScene("End Scene"); // same as above, different scene
+    }
+}
+
