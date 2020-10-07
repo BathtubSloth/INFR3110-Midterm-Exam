5d5f709719c9b9435d185f2ead1459054136440d
Nathan Tuck <nathan.tuck1@ontariotechu.net>
2020-10-07 14:42:43 -0400

    Added basic character controller
    Character controller doesn't integrate with the DLL yet but hey it moves

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Security.Permissions;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if(Input.GetKey(KeyCode.W))
+        { 
+            transform.Translate(0, 0, 1 * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-1 * moveSpeed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -1 * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(1 * moveSpeed, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveSpeed = moveSpeed * 2;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed = moveSpeed / 2;
+        }
+    }
+}
