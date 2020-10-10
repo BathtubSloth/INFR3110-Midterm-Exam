130c31087ad5bdd47a98a69aac8ad5def1459275
Nathan Tuck <nathan.tuck1@ontariotechu.net>
2020-10-09 21:27:46 -0400

    Small Optimization
    Small optimization that is not reflected in the video but shouldn't change any functionality. I was really tired when I wrote the original code so I figured I should fix it

--- Assets/Scripts/TimeDisplay.cs
+++ Assets/Scripts/TimeDisplay.cs
@@ -21,13 +21,7 @@ public class TimeDisplay : MonoBehaviour
     void Update()
     {
         UpdateTimer();
-        UpdateSplit1();
-        UpdateSplit2();
-        UpdateSplit3();
-        UpdateSplit4();
-        UpdateSplit5();
-        TotalTime();
-        //DontDestroyOnLoad(splitDisplay1);
+        UpdateSplit();
     }
 
     //This function updates the timer to display in a minutes:seconds:milliseconds to 2 decimal places
@@ -42,34 +36,14 @@ public class TimeDisplay : MonoBehaviour
         }
     }
 
-    // Super unoptimized and hardcoded but I'm just trying to finish here
-    public void UpdateSplit1()
+    // OPTIMIZED THIS A LITTLE AFTER SUBMITTING THE VIDEO SO THIS IS SLIGHTLY DIFFERENT FROM WHAT'S CONTAINED IN THE VIDEO
+    public void UpdateSplit()
     {
         splitDisplay1.text = "Checkpoint 1: " + PluginHandler.LoadTimeTest(1).ToString("f2");
-    }
-
-    public void UpdateSplit2()
-    {
         splitDisplay2.text = "Checkpoint 2: " + PluginHandler.LoadTimeTest(2).ToString("f2");
-    }
-
-    public void UpdateSplit3()
-    {
         splitDisplay3.text = "Checkpoint 3: " + PluginHandler.LoadTimeTest(3).ToString("f2");
-    }
-
-    public void UpdateSplit4()
-    {
         splitDisplay4.text = "Checkpoint 4: " + PluginHandler.LoadTimeTest(4).ToString("f2");
-    }
-
-    public void UpdateSplit5()
-    {
         splitDisplay5.text = "Final Time: " + PluginHandler.LoadTimeTest(5).ToString("f2");
-    }
-
-    public void TotalTime()
-    {
         splitDisplay6.text = "Total Time: " + PluginHandler.LoadTotalTimeTest().ToString("f2");
     }
 }
